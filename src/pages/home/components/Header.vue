7b0ca6b323434c962eec6a0f96a4b76f63ccbc14
sunwuquan <15064777789@163.com>
2019-01-08 18:30:44 +0800

    add swiper and header

---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="header">
+    <div class="header-left"><span class="iconfont back-icon">&#xe624;</span></div>
+    <div class="header-input" contenteditable="true"><span class="iconfont">&#xe632;</span>输入城市/景点/游玩主题</div>
+    <div class="header-right">城市<span class="iconfont arrow-icon">&#xe64a;</span></div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+<style scoped lang="stylus">
+@import "~@/assets/styles/varibles.styl"
+  .header
+    display flex
+    line-height .86rem
+    color #fff
+    background $bgColor
+    .header-left
+      width .64rem
+      float left
+      .back-icon
+        display block
+        text-align center
+        font-size .4rem
+    .header-input
+      flex 1
+      color #ccc
+      height .64rem
+      line-height .64rem;
+      background #ffffff
+      border-radius .1rem
+      margin-top .12rem
+      padding-left .2rem
+      height .64rem;
+      margin-left .2rem
+    .header-right
+      width 1.24rem;
+      text-align center
+      float right
+      .arrow-icon
+        font-size .24rem
+</style>
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+      <swiper-slide v-for="item in swiperList" :key="item.id">
+        <img class="swiper-img" :src="item.imgUrl" alt="">
+      </swiper-slide>
+      <div class="swiper-pagination" slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperList: [
+        {id: 1, imgUrl: 'http://img1.qunarzz.com/piao/fusion/1809/12/856f100069809e02.jpg_750x200_e3485a2b.jpg'},
+        {id: 2, imgUrl: 'http://img1.qunarzz.com/piao/fusion/1812/2f/49f6152a86eb4502.jpg_750x200_f4841fa3.jpg'},
+        {id: 3, imgUrl: 'http://img1.qunarzz.com/piao/fusion/1811/f7/72ce947f4dff4c02.jpg_750x200_aa583dba.jpg'},
+        {id: 4, imgUrl: 'http://img1.qunarzz.com/piao/fusion/1810/af/30ffcf34e9819f02.jpg_750x200_736b7236.jpg'}
+      ]
+    }
+  }
+}
+</script>
+<style lang="stylus" scoped>
+  .wrapper >>> .swiper-pagination-bullet-active
+    background #fff
+  .wrapper
+    width 100%;
+    height 0;
+    overflow hidden;
+    background #ccc;
+    padding-bottom 26.66%
+    .swiper-img
+      width 100%
+</style>
